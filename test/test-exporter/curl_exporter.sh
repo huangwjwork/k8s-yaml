956a3d00f1adf3a72e28e7faec33c1b2322f318a
huangwjwork <huangwjwork@gmail.com>
2019-08-02 11:09:44 +0800

    save to github

---
+++ test/test-exporter/curl_exporter.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+while true; do curl 10.105.124.110:10000 -o /dev/null ; done 
+
---
+++ test/test-exporter/test_exporter.py
@@ -0,0 +1,16 @@
+#!/bin/env python3
+from prometheus_client import Gauge, start_http_server
+import random
+import time
+test_metric = Gauge('test_metric', '测试serviceMonitor')
+jvm_metrics = Gauge('jvm_memory_use', '模拟jvm数据')
+http_request_per_second = Gauge('http_request_per_second', '模拟http请求数据')
+
+if __name__ == "__main__":
+    start_http_server(10000)
+    while True:
+        for i in range(0, 10):
+            test_metric.set(random.random())
+            jvm_metrics.set(i)
+            http_request_per_second.set(i * 1000)
+            time.sleep(120)
